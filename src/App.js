e3bb40636f81163a4c9de0bb737adae43d8f22e0
zaviyar-dev <83871709+zaviyar-dev@users.noreply.github.com>
2022-01-08 00:15:40 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./Routes/Home";
 import CoinPage from "./Routes/CoinPage";
 
+
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Route path="/" exact render={(props) => <Home />} />
+      <Switch>
+        <Route exact path="/" component={Home}/>
         <Route path="/CoinPage/:id" exact render={(props) => <CoinPage />} />
-      </Router>
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import App from './App';
 import BrowserRouter  from "react-router-dom/BrowserRouter";
 
 ReactDOM.render(
-  <React.StrictMode>
   <BrowserRouter basename={process.env.PUBLIC_URL}>
    <App />
   </BrowserRouter>
-    
-  </React.StrictMode>,
+    ,
   document.getElementById('root')
 );
 
